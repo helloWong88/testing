99b443d9101e4c31c136cba920a710db5a889933
16215923 <16215923@cs12195.comp.hkbu.edu.hk>
2018-10-03 16:44:03 +0800

    improve the view

--- api/controllers/PersonController.js
+++ api/controllers/PersonController.js
@@ -30,14 +30,16 @@ index: async function (req, res) {
 // action - view
 view: async function (req, res) {
 
-    var pid = parseInt(req.params.id) || -1;
-    
-    var model = await Person.findOne(pid);
-    
-    if (model != null)
-        return res.view('person/view', { 'person': model });
-    else
-        return res.send("No such person");
+    var message = Person.getInvalidIdMsg(req.params);
+
+    if (message) return res.badRequest(message);
+
+    var model = await Person.findOne(req.params.id);
+
+    if (!model) return res.notFound();
+
+    return res.view('person/view', { 'person': model });
+
 },
 // action - delete 
 delete: async function (req, res) {
